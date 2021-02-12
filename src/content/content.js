5454a26022ce97aee387779888d39ffaf56e9406
Dave Stewart <dave@davestewart.co.uk>
2021-02-12 03:15:22 +0000

    Major stability refactor

--- src/content/content.js
+++ src/content/content.js
@@ -1,21 +1,11 @@
-/* eslint-disable no-unused-vars */
-// ---------------------------------------------------------------------------------------------------------------------
-// HELPERS
-// ---------------------------------------------------------------------------------------------------------------------
+// imports
 import './content.scss'
-
 import Page from './classes/Page.js'
-import Data from './classes/Data.js'
 import App from './classes/App.js'
-
-// ---------------------------------------------------------------------------------------------------------------------
-// MAIN
-// ---------------------------------------------------------------------------------------------------------------------
 
 // instances
 const page = new Page()
-const data = new Data()
-const app = new App(page, data)
+const app = new App(page)
 
 // debug
 console.log('MultiFlow is ready...')
@@ -23,14 +13,14 @@ console.log('MultiFlow is ready...')
 // commands
 chrome.runtime.onMessage.addListener(function (request = {}, _sender, callback) {
   switch (request.command) {
-    case 'start':
-      return callback(app.start())
+    case 'init':
+      return callback(app.init(request.value))
 
-    case 'setLayout':
-      return callback(app.setLayout(request.value))
+    case 'getData':
+      return callback(app.getData())
 
-    case 'getLayout':
-      return callback(app.getLayout())
+    case 'setData':
+      return callback(app.setData(request.value))
 
     default:
       // eslint-disable-next-line node/no-callback-literal
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,73 +1,148 @@
 import './popup.scss'
 import { runCommand } from './utils.js'
-
-// import Vue from 'vue'
+import { storage } from '../utils/storage.js'
+import { getTitle } from '../utils/common.js'
 
 window.app = new Vue({
   el: '#app',
 
   data () {
     return {
-      clicked: false,
-      links: 'on-right',
-      layout: 'fit-screen',
-      sessionIndex: -1,
-      sessions: [
-        { id: 1, title: 'Session 01' },
-        { id: 2, title: 'Session 02' },
-      ],
+      settings: {
+        layout: 'fit-screen',
+        links: 'in-place',
+      },
+      sessions: {
+        current: null,
+        saved: null,
+      },
+      /*
+      options: {
+        session: null,
+        sessionIndex: -1,
+        sessions: [
+          { id: 1, title: 'Session 01' },
+          { id: 2, title: 'Session 02' },
+        ],
+      },
+      */
     }
   },
 
   computed: {
+    currentSessionTitle () {
+      return getTitle(this.sessions.current)
+    },
+
+    savedSessionTitle () {
+      return getTitle(this.sessions.saved)
+    },
+
+    /*
     session () {
-      return this.sessions[this.sessionIndex]
+      return this.options.sessions[this.options.sessionIndex]
     },
 
     hasSessions () {
-      return this.sessions.length > 0
+      return this.options.sessions.length > 0
+    },
+
+    hasUnloadedSession () {
+      return true
     },
 
     noSelection () {
-      return this.sessionIndex === -1
+      return this.options.sessionIndex === -1
     },
-  },
-
-  watch: {
-    layout: 'setLayout',
+    */
   },
 
   mounted () {
-    this.start()
+    // show
+    setTimeout(() => {
+      document.querySelector('.main').classList.remove('hidden')
+    }, 100)
 
-    document.body.addEventListener('mouseleave', () => {
-      if (this.clicked) {
-        // window.close()
-      }
+    // set up watch
+    Object.keys(this.settings).forEach(key => {
+      this.$watch(`settings.${key}`, value => this.setData(key, value))
     })
+
+    // listen for any messages
+    chrome.runtime.onMessage.addListener(this.onMessage)
+
+    // initialize!
+    return this.init()
+  },
+
+  beforeDestroy () {
+    chrome.runtime.onMessage.removeListener(this.onMessage)
   },
 
   methods: {
-    async start () {
-      // run the page start script
-      await runCommand('start')
-
-      // get layout
-      const layout = await runCommand('getLayout')
-      if (layout) {
-        this.layout = layout
+    async init () {
+      // always load previous session
+      const session = storage.get('session')
+      if (Array.isArray(session)) {
+        this.sessions.saved = session
       }
+
+      // need to also load settings here
+      const settings = storage.get('settings')
+
+      // run the page start script
+      await runCommand('init', settings)
+
+      // get settings from page
+      const data = await this.getData()
+      this.settings.links = data.links
+      this.settings.layout = data.layout
+      this.sessions.current = data.frames
     },
 
-    setLayout (value) {
-      console.log(value)
-      runCommand('setLayout', this.layout).then(() => {
-        this.clicked = true
+    // ---------------------------------------------------------------------------------------------------------------------
+    // body data
+    // ---------------------------------------------------------------------------------------------------------------------
+
+    setData (key, value) {
+      return runCommand('setData', { [key]: value }).then(() => {
+        storage.set('settings', this.settings)
       })
     },
 
-    close () {
+    getData () {
+      return runCommand('getData')
+    },
 
+    // ---------------------------------------------------------------------------------------------------------------------
+    // sessions
+    // ---------------------------------------------------------------------------------------------------------------------
+
+    loadSession () {
+      const session = storage.get('session')
+      if (Array.isArray(session)) {
+        this.sessions.current = session
+        const urls = session.map(frame => frame.url)
+        return this.setData('urls', urls)
+      }
+    },
+
+    async saveSession () {
+      storage.set('session', this.sessions.current)
+      this.sessions.saved = this.sessions.current
+    },
+
+    // ---------------------------------------------------------------------------------------------------------------------
+    // utilities
+    // ---------------------------------------------------------------------------------------------------------------------
+
+    reload () {
+      window.location.reload()
+    },
+
+    onMessage (message) {
+      alert(JSON.stringify(message))
+      console.log(message)
     },
   },
 })
